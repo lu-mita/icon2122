15feb892a2182339f8aca2b74ee201c28d133f58
Nicolin Damalja <damaljanicolin@gmail.com>
2022-11-08 15:43:48 +0100

    Nuove aggiunte e modifiche

--- branch_and_bound.py
+++ branch_and_bound.py
@@ -1,157 +1,97 @@
-# -*- coding: utf-8 -*-
-"""
-0-1 Knapsack Problem using best first branch and bound method
-Returns maxprofit with list storing the index position of the items in the best solution.
-The profit is maximized while staying under the weight limit.
-This program uses a priority queue to store the nodes ordered by best bound,
-the node with the highest bound value is returned when removing from the priority queue.
-The best first approach arrives at an optimal solition faster than breadth first search.
-"""
-
-# items are ordered by price per weight
+from queue import Queue
+from collections import namedtuple
 import pandas as pd
 
-df = pd.read_csv('Output_.csv')
-n = 100
-W = 80
-p = df["UTILE"]
-w = df["PESO VOLUMETRICO"]
-p_per_weight = p/w
-food = list(df["PRODOTTO"])
-
-
-class Priority_Queue:
-    def __init__(self):
-        self.pqueue = []
-        self.length = 0
-
-    def insert(self, node):
-        for i in self.pqueue:
-            get_bound(i)
-        i = 0
-        while i < len(self.pqueue):
-            if self.pqueue[i].bound > node.bound:
-                break
-            i += 1
-        self.pqueue.insert(i, node)
-        self.length += 1
-
-    def print_pqueue(self):
-        for i in list(range(len(self.pqueue))):
-            print("pqueue", i, "=", self.pqueue[i].bound)
-
-    def remove(self):
-        try:
-            result = self.pqueue.pop()
-            self.length -= 1
-        except:
-            print("Priority queue is empty, cannot pop from empty list.")
-        else:
-            return result
-
-
 class Node:
-    def __init__(self, level, profit, weight):
-        self.level = level
-        self.profit = profit
-        self.weight = weight
-        self.items = []
+    def __init__(self):
+        self.level = None
+        self.profit = None
+        self.bound = None
+        self.weight = None
+
+    def __str__(self):
+        return "Level: %s Profit: %s Bound: %s Weight: %s" % (self.level, self.profit, self.bound, self.weight)
 
 
-def get_bound(node):
+def bound(node, n, W, items):
     if node.weight >= W:
         return 0
-    else:
-        result = node.profit
-        j = node.level + 1
-        totweight = node.weight
-        while j <= n - 1 and totweight + w[j] <= W:
-            totweight = totweight + w[j]
-            result = result + p[j]
-            j += 1
-        k = j
-        if k <= n - 1:
-            result = result + (W - totweight) * p_per_weight[k]
-        return result
 
+    profit_bound = int(node.profit)
+    j = node.level + 1
+    totweight = int(node.weight)
 
-nodes_generated = 0
-pq = Priority_Queue()
+    while (j < n) and (totweight + items[j].weight) <= W:
+        totweight += items[j].weight
+        profit_bound += items[j].value
+        j += 1
 
-v = Node(-1, 0, 0)  # v initialized to be the root with level = 0, profit = $0, weight = 0
-nodes_generated += 1
-maxprofit = 0  # maxprofit initialized to $0
-v.bound = get_bound(v)
-# print("v.bound = ", v.bound)
+    if(j < n):
+        profit_bound += (W - totweight) * items[j].value / float(items[j].weight)
 
+    return profit_bound
 
-pq.insert(v)
-bestitems = None
+Q = Queue()
 
-while pq.length != 0:
+def KnapSackBranchNBound(weight, items, total_items):
+    items = sorted(items, key=lambda x: x.value/float(x.weight), reverse=True)
 
-    v = pq.remove()  # remove node with best bound
-    #    print("\nNode removed from pq.")
-    #    print("Priority Queue: ")
-    #    pq.print_pqueue()
+    u = Node()
+    v = Node()
 
-    #    print("\nmaxprofit = ", maxprofit)
-    #    print("Parent Node: ")
-    #    print("v.level = ", v.level, "v.profit = ", v.profit, "v.weight = ", v.weight, "v.bound = ", v.bound, "v.items = ", v.items)
+    u.level = -1
+    u.profit = 0
+    u.weight = 0
 
-    if v.bound > maxprofit:  # check if node is still promising
-        # set u to the child that includes the next item
-        u = Node(0, 0, 0)
-        nodes_generated += 1
-        u.level = v.level + 1
-        u.profit = v.profit + p[u.level]
-        u.weight = v.weight + w[u.level]
-        # take v's list and add u's list
-        u.items = v.items.copy()
-        u.items.append(u.level)  # adds next item
-        #        print("child that includes the next item: ")
-        #        print("Child 1:")
-        #        print("u.level = ", u.level, "u.profit = ", u.profit, "u.weight = ", u.weight)
-        #        print("u.items = ", u.items)
-        if u.weight <= W and u.profit > maxprofit:
-            # update maxprofit
-            maxprofit = u.profit
-            #            print("\nmaxprofit updated = ", maxprofit)
-            bestitems = u.items
-        #            print("bestitems = ", bestitems)
-        u.bound = get_bound(u)
-        #        print("u.bound = ", u.bound)
-        if u.bound > maxprofit:
-            pq.insert(u)
-        #            print("Node u1 inserted into pq.")
-        #            print("Priority Queue : ")
-        #            pq.print_pqueue()
-        # set u to the child that does not include the next item
-        u2 = Node(u.level, v.profit, v.weight)
-        nodes_generated += 1
-        u2.bound = get_bound(u2)
-        u2.items = v.items.copy()
-        #        print("child that doesn't include the next item: ")
-        #        print("Child 2:")
-        #        print("u2.level = ", u2.level, "u2.profit = ", u2.profit, "u2.weight = ", u2.weight, "u2.bound = ", u2.bound)
-        #        print("u2.items = ", u2.items)
-        if u2.bound > maxprofit:
-            pq.insert(u2)
-#            print("Node u2 inserted into pq.")
-#            print("Priority Queue : ")
-#            pq.print_pqueue()
+    Q.put(u)
+    maxProfit = 0
 
+    while not Q.empty():
+        u = Q.get()
+        v = Node()  # Added line
+        if u.level == -1:
+            v.level = 0
 
-print("\nEND maxprofit = ", maxprofit, "nodes generated = ", nodes_generated)
-print("Best items=[", end=" ")
-for i in range(len(bestitems)):
-    for j in range(len(food)):
-        if i < len(bestitems)-1:
-            if bestitems[i] == j:
-                print(food[j], end=", ")
-            pass
-        elif bestitems[i] == j and i == len(bestitems)-1:
-            print(food[j], "]")
+        if u.level == total_items - 1:
+            continue
+
+        v.level = u.level + 1
+        v.weight = u.weight + items[v.level].weight
+        v.profit = u.profit + items[v.level].value
+        if v.weight <= weight and v.profit > maxProfit:
+            maxProfit = v.profit
+
+        v.bound = bound(v, total_items, weight, items)
+        if v.bound > maxProfit:
+            Q.put(v)
+
+        v = Node()  # Added line
+        v.level = u.level + 1  # Added line
+        v.weight = u.weight
+        v.profit = u.profit
+        v.bound = bound(v, total_items, weight, items)
+        if v.bound > maxProfit:
+            # print(items[v.level])
+            Q.put(v)
+
+    return maxProfit
+
+def execute(filename):
+    capacity = int(input("Insert truck capacity:"))
+    item_count = 100
+    Item = namedtuple("Item", ['index', 'value', 'weight'])
+    file = pd.read_csv(filename)
+    weights = list(file["PESO VOLUMETRICO"])
+    values = list(file["UTILE"])
+
+    items = []
+
+    for i in range(1, item_count + 1):
+        items.append(Item(i - 1, values[i - 1], weights[i - 1]))
+    kbb = KnapSackBranchNBound(capacity, items, item_count)
+    print(f"Utile massimo:{kbb}€\n")
+
+execute('Products.csv')
 
 
 
